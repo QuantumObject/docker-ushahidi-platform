dbda136348748d44dbf457626761a398454722d4
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-11-25 07:51:48 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+/usr/bin/mysqld_safe &
+ sleep 10s
 
-#need to be fix for container enviroment including running database while it is modified it 
-mkdir /var/www/ushahidi-dev
-cd /var/www/ushahidi-dev/
-git clone https://github.com/ushahidi/platform.git
-chmod 770 application/config/config.php
-chmod 770 application/config
-chmod 770 application/cache
-chmod 770 application/logs
-chmod 770 media/uploads
-chmod 770 .htaccess
-sudo mysqladmin -u root --password create ushahidi-dev
+ mysqladmin -u root password mysqlpsswd
+ mysqladmin -u root -pmysqlpsswd reload
+ mysqladmin -u root -pmysqlpsswd create ushahidi
 
-#reason of this script is that dockerfile only execute one command at the time but we need sometimes at the moment we create 
-#the docker image to run more that one software for expecified configuration like when you need mysql running to chnage or create
-#database for the container ...
+ echo "GRANT ALL ON ushahidi.* TO ushahidiuser@localhost IDENTIFIED BY 'ushahidipasswd'; flush privileges; " | mysql -u root -pmysqlpsswd
+
+ #need to be fix for container enviroment including running database while it is modified it 
+ mkdir /var/www/ushahidi
+ cd /var/www/ushahidi/
+ git clone https://github.com/ushahidi/platform.git
+ chmod 770 application/config/config.php
+ chmod 770 application/config
+ chmod 770 application/cache
+ chmod 770 application/logs
+ chmod 770 media/uploads
+ chmod 770 .htaccess
+ rm -R /var/www/html
+ a2enmod rewrite
+
+
+killall mysqld
+sleep 10s

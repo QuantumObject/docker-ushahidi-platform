86ae04c3b3cdacfb107f5e588d7df3204871137d
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-11-25 09:53:48 -0500

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -13,13 +13,13 @@
  cd /var/www/
  git clone https://github.com/ushahidi/platform.git
  #git submodule update --init --recursive
- chown -R www-data:www-data /var/www/ushahidi
- chmod 770 platform/application/config/config.php
- chmod 770 platform/application/config
+ chown -R www-data:www-data /var/www/platform
+ #chmod 770 platform/application/config/config.php
+ #chmod 770 platform/application/config
  chmod 770 platform/application/cache
  chmod 770 platform/application/logs
  chmod 770 platform/application/media/uploads
- chmod 770 platform/.htaccess
+ #chmod 770 platform/.htaccess
  rm -R /var/www/html
  a2enmod rewrite
 

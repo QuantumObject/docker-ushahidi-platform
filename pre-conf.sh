2802f8a95d49ae963762498660e30c0a42966d94
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-09-15 21:17:48 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -15,9 +15,13 @@ mysql_install_db
  phpenmod mcrypt
  phpenmod imap
  phpenmod mysqli 
+ 
  COOKIE_SALT=`pwgen -c -n -1 32`
  cd /var/www/
- git clone https://github.com/ushahidi/platform.git
+ wget https://github.com/ushahidi/platform/archive/v3.12.3.tar.gz
+ tar -xvf v3.12.3.tar.gz
+ rm v3.12.3.tar.gz
+ mv platform-3.12.3 platform 
  
  curl -sS https://getcomposer.org/installer | php
  mv composer.phar /usr/local/bin/composer
@@ -35,16 +39,7 @@ DB_TYPE=MySQLi
  /var/www/platform/bin/update
  cd /var/www/
  chown -R www-data:www-data /var/www/platform
- mv /var/www/platform/httpdocs/template.htaccess /var/www/platform/httpdocs/.htaccess
- mkdir -p platform/application/config/environments/development
- cp platform/application/config/init.php platform/application/config/environments/development/
- sed  -i "s/'index_file'  => FALSE,/'index_file'  => 'index.php',/" platform/application/config/environments/development/init.php
- # Reset the default cookie salt to something unique
- sed -i -e "s/Cookie::\$salt = '.*';/Cookie::\$salt = '$COOKIE_SALT';/" platform/application/bootstrap.php 
  
- chown -R www-data:www-data platform/application/{logs,cache,media/uploads}
- chmod 755 platform/httpdocs/.htaccess
-
  rm -R /var/www/html
  
   #to fix error relate to ip address of container apache2

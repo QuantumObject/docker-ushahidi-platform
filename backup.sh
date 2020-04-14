87e82af96262899c1864bec637a69109b19a38ba
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-04-14 12:02:38 -0400

    Delete backup.sh

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this for reference only to create the backup scrips for each container ... the idea to use the same command for each container
-#each container will have their own custum backup scritp for it ... 
-
-#Backup mysql
-mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
-
-#Backup importand files relate to app
